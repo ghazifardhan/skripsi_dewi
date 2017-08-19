1b8f910b5238c853b6b34bce1aaa6ee09a9bca0d
Ghazi Fadil <ghazifardhan@gmail.com>
2017-08-19 17:44:58 +0700

    rev del cust

--- delete_customer.php
+++ delete_customer.php
@@ -1,5 +1,5 @@
 <?php
 include_once('koneksi.php');
-$query=mysql_query("delete from customer where Id_Customer='".$_GET['Id_Customer']."'") or die (mysql_error());
+$query=mysqli_query($GLOBALS["___mysqli_ston"], "delete from customer where Id_Customer='".$_GET['Id_Customer']."'") or die (mysql_error());
 if($query)header("location:tampilan_customer.php");else die (mysql_error());
 ?>
